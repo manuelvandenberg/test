a35ec6f2dad71739c24673db8021449dcda84744
Manuel van den Berg <manuel.vandenberg@humaninference.com>
2015-05-13 16:21:47 +0200

    SBII-1 #in-progress work started

---
+++ test3.java
@@ -0,0 +1,8 @@
+class test {
+    // Nono wants comments, so here you go...
+    public static void main( String[] args ) {
+        System.out.println( "Mwahah!!!" );
+    }
+}
+// Jajajaja
+// Definitely a conflict
