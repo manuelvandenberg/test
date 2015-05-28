ea31aa4f93228541cae2f10b2273a85cb00d70f8
Manuel van den Berg <manuel.vandenberg@humaninference.com>
2015-05-28 17:30:02 +0200

    Initial work on SB-14 #start

--- test.java
+++ test.java
@@ -1,8 +1,9 @@
+// Some useful doc should be here.
+
 class test {
     // Nono wants comments, so here you go...
     public static void main( String[] args ) {
         System.out.println( "Mwahah!!!" );
+        System.exit(1);
     }
 }
-// haha 
-// yo
